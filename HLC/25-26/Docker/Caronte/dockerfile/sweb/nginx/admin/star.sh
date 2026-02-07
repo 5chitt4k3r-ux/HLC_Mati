5a58faa9a46898ccd8fc0f7669d76cd55d7c4a86
Matias <5chitt4k3r@gmail.com>
2026-02-07 13:23:05 +0100

    a

--- HLC/25-26/Docker/Caronte/dockerfile/sweb/nginx/admin/star.sh
+++ HLC/25-26/Docker/Caronte/dockerfile/sweb/nginx/admin/star.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 
-config_nginx() {
-    nginx
+load_entrypoint_base() {
+    bash /root/admin/base/start.sh
 }
+
+reiniciar_nginx() {
+    echo "Configurando Nginx..." >> /root/logs/informe_nginx.log
+# Reiniciar el servicio de Nginx para aplicar los cambios
+    if service nginx restart; then
+        echo "Nginx reiniciado" >> /root/logs/informe_nginx.log
+    else
+        echo "ERROR: No se pudo reiniciar Nginx" >> /root/logs/informe_nginx.log
+    fi
+# Detener el servicio de Nginx para evitar que se ejecute en segundo plano
+    if service nginx stop; then
+       echo "Nginx detenido" >> /root/logs/informe_nginx.log
+    else
+       echo "ERROR: No se pudo detener Nginx" >> /root/logs/informe_nginx.log
+    fi
+}
+
+
+main() {
+    mkdir -p /root/logs
+    touch /root/logs/informe_nginx.log
+    load_entrypoint_base
+    reiniciar_nginx
+    tail -f /dev/null
+}
+
+main
 
